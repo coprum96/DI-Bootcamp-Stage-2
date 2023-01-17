b59e8f37e4b2f734215b0bde20bc68de9b1d4f8e
coprum96 <medn@list.ru>
2023-01-17 15:46:43 +0300

    exercise XP

--- Week 2/Day 1/Exercises XP/script.js
+++ Week 2/Day 1/Exercises XP/script.js
@@ -46,10 +46,22 @@ console.log('I watched ' + myWatchedSeriesLength + ' series: ' + myWatchedSeries
 
 // Change the series “the big bang theory” to “friends”
 
+const index = myWatchedSeries.indexOf('the big bang theory');
+myWatchedSeries.splice(index, 1, 'friends');
+
 
 myWatchedSeries.push('lost')
-const myLastSeries = myLastSeries.indexOf('the big bang theory');
-myLastSeries = myLastSeries.indexOf('friends')
 
-console.log('I watched ' + myWatchedSeriesLength + ' series: '+ myWatchedSeries + ' and ' + myLastSeries)
+console.log('I watched ' + myWatchedSeriesLength + ' series: '+ myWatchedSeries)
 
+
+// Exercise 3
+// 1. Store a celsius temperature into a variable.
+
+// 2. Convert it to fahrenheit and console.log <temperature>°C is <temperature>°F.
+// Hint : Should you create another variable to hold the temperature in fahrenheit? (ie. point 2)
+// Hint: To convert a temperature from celsius to fahrenheit : Divide it by 5, then multiply it by 9, then add 32
+
+let celsiusTemperatur = 20
+let fahrenheitTemperatur = (celsiusTemperatur / 5) * 9 + 32
+console.log(fahrenheitTemperatur)
