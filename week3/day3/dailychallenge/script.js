15379099dade946429163e51fb17ea2333bf06b4
coprum96 <medn@list.ru>
2023-01-31 22:36:01 +0300

    daily challenge

---
+++ week3/day3/dailychallenge/script.js
@@ -0,0 +1,21 @@
+// Instructions
+// Daily Challenge : Show Only The Letters
+// Create an input type text that takes/shows only letters. (ie. numbers and special characters won’t be accepted)
+
+// Hint: use one of the following events to remove any character that is not a letter
+
+// keyup event
+// or keypress event
+// or keydown event
+// or input event
+
+const lettersInput = document.querySelector("#letterInput");
+
+
+lettersInput.addEventListener("keyup", onlyAlphabet)
+
+
+function onlyAlphabet(event) {
+    event.target.value = event.target.value.replace(/[^a-zA-Z]/g, "");
+  }
+  
