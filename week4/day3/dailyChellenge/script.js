7dea7008c2d6a645f70842b77f3015e3df575c93
coprum96 <medn@list.ru>
2023-02-08 08:56:47 +0300

    new

--- week4/day3/dailyChellenge/script.js
+++ week4/day3/dailyChellenge/script.js
@@ -26,3 +26,32 @@ function getCarHonda(carInventory) {
 }
 
 console.log(getCarHonda(inventory));
+
+
+// Part II
+
+    // Create a function sortCarInventoryByYear(carInventory) that takes a single parameter. carInventory‘s value is an array which is an inventory of cars (see the array of objects below)
+    // the function should return an inventory that is sorted by car_year, ascending.
+    // Hint : Check out this tutorial on the sort method
+    // Use the cars inventory below:
+  
+    const inventory = [
+        { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+        { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+        { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+        { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+        { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+      ];
+        
+    //    function sortCarInventoryByYear(carInventory) {
+    //     const carInventory = inventory.map;
+    //     const sortedYears= carInventory.sort((a, b) => a.car_year - b.car_year);
+    //     return sortedYears
+    //    }
+    //    sortCarInventoryByYear(1985)
+
+
+
+        const sortedYears = inventory.sort((a, b) => a.car_year - b.car_year);
+        console.log(sortedYears)
+      
