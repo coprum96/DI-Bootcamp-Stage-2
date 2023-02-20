b22ad6765ea8f65bea550ee645273a1de08aa0d1
coprum96 <medn@list.ru>
2023-02-20 15:16:53 +0300

    new

---
+++ week6/day2/xp/public.sql
@@ -0,0 +1,7 @@
+-- SELECT * FROM customers;
+-- SELECT * FROM items;
+-- SELECT * FROM items ORDER BY price ASC;
+-- SELECT * FROM items WHERE price >=80 ORDER BY price DESC;
+
+-- SELECT first_name FROM customers ORDER BY first_name ASC;
+-- SELECT last_name FROM customers ORDER BY last_name DESC;
