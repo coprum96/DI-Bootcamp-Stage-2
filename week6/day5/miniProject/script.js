8ca65e61361067895435a84b2e9be3111d09a367
coprum96 <medn@list.ru>
2023-02-23 15:03:53 +0300

    new big

--- week6/day5/miniProject/script.js
+++ week6/day5/miniProject/script.js
@@ -1,63 +1,31 @@
-const nameInput = document.getElementById('name');
-const descriptionInput = document.getElementById('description');
-const startInput = document.getElementById('start');
-const endInput = document.getElementById('end');
-const addButton = document.getElementById('addTaskButton');
+// get references to form and submit button
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('a');
 
-let tasks = [];
-
-function displayTasks() {
-  const taskList = document.getElementById('taskList');
-  taskList.innerHTML = '';
-  for (let i = 0; i < tasks.length; i++) {
-    const task = tasks[i];
-    const li = document.createElement('li');
-    li.textContent = `${task.name} - ${task.description} - ${task.start.toLocaleString()} to ${task.end.toLocaleString()}`;
-    
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.addEventListener('click', () => {
-      tasks.splice(i, 1);
-      localStorage.setItem('tasks', JSON.stringify(tasks));
-      displayTasks();
-    });
-    
-    li.appendChild(deleteButton);
-    taskList.appendChild(li);
-  }
-}
-
-function addTask() {
-    const name = nameInput.value;
-    const description = descriptionInput.value;
-    const start = new Date(startInput.value);
-    const end = new Date(endInput.value);
-    const isCompleted = false;
-    
-    if (name && description && start && end) {
-      const task = {
-        name: name,
-        description: description,
-        start: start,
-        end: end,
-        isCompleted: isCompleted
-      };
-      tasks.push(task);
-      localStorage.setItem('tasks', JSON.stringify(tasks));
-      nameInput.value = '';
-      descriptionInput.value = '';
-      startInput.value = '';
-      endInput.value = '';
-      displayTasks();
-    }
-  }
+// add event listener to submit button
+submitBtn.addEventListener('click', function(e) {
+  e.preventDefault(); // prevent form from submitting normally
   
-addButton.addEventListener('click', addTask);
-
-window.addEventListener('load', () => {
-  const storedTasks = localStorage.getItem('tasks');
-  if (storedTasks) {
-    tasks = JSON.parse(storedTasks);
-    displayTasks();
-  }
+  // get form values
+  const name = document.querySelector('#name').value;
+  const description = document.querySelector('#description').value;
+  const start = new Date(document.querySelector('#start').value).getTime();
+  const end = new Date(document.querySelector('#end').value).getTime();
+  const done = document.querySelector('#done').value
+  // create task object
+  const task = {
+    name,
+    description,
+    start,
+    end,
+    done
+  };
+  
+  // save task to local storage
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  
+  // redirect to tasks page
+  window.location.href = 'tasks.html';
 });
--- week6/day5/miniProject/tasks.js
+++ week6/day5/miniProject/tasks.js
@@ -1,44 +1,78 @@
-const taskList = document.getElementById('task-list');
+// get tasks from local storage
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-function displayTasks() {
-  taskList.innerHTML = '';
-  for (let i = 0; i < tasks.length; i++) {
-    const task = tasks[i];
-    
-    const li = document.createElement('li');
-    const name = document.createElement('span');
-    const startDate = document.createElement('span');
-    const remainingDays = document.createElement('span');
-    
-    name.textContent = task.name;
-    startDate.textContent = task.start.toDateString();
-    remainingDays.textContent = getRemainingDays(task.end);
-    
-    li.appendChild(name);
-    li.appendChild(startDate);
-    li.appendChild(remainingDays);
-    
-    taskList.appendChild(li);
-  }
-}
-
-function getRemainingDays(date) {
-  const today = new Date();
-  const daysLeft = Math.ceil((date.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
-  
-  if (daysLeft > 0) {
-    return ` - ${daysLeft} days left to complete the task.`;
-  } else if (daysLeft === 0) {
-    return ` - Today is the last day to complete the task.`;
-  } else {
-    return ` - Task is already overdue by ${Math.abs(daysLeft)} days.`;
-  }
-}
-
-window.addEventListener('load', () => {
-  const storedTasks = localStorage.getItem('tasks');
-  if (storedTasks) {
-    tasks = JSON.parse(storedTasks);
-    displayTasks();
-  }
+// sort tasks by start date
+tasks.sort(function(a, b) {
+  return a.start - b.start;
 });
+
+// display tasks in list
+const taskList = document.querySelector('#task-list');
+tasks.forEach(function(task, index) {
+  const li = document.createElement('li');
+  const name = document.createElement('h2');
+  const description = document.createElement('p');
+  const start = document.createElement('p');
+  const end = document.createElement('p');
+  const done = document.createElement('p')
+  const deleteButton = document.createElement('button');
+  const editButton = document.createElement('button');
+  
+  name.textContent = 'Name:' + task.name;
+  description.textContent = 'Description: ' + task.description;
+  start.textContent = 'Start date and time: ' + new Date(task.start).toLocaleString();
+  end.textContent = ' End date and time: ' + new Date(task.end).toLocaleString();
+  done.textContent = 'Done: ' + task.done;
+  deleteButton.textContent = 'Delete the task'
+  editButton.textContent = 'Edit';
+  
+  li.appendChild(name);
+  li.appendChild(description);
+  li.appendChild(start);
+  li.appendChild(end);
+  li.appendChild(done);
+  li.appendChild(deleteButton);
+  li.appendChild(editButton);
+
+  deleteButton.addEventListener('click', function() {
+    tasks.splice(index, 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    li.remove();
+  });
+
+  editButton.addEventListener('click', function() {
+    const newName = prompt('Enter a new name:', task.name);
+    if (newName) {
+      task.name = newName;
+      const newDescription = prompt('Enter a new description:', task.description);
+      if (newDescription) {
+        task.description = newDescription;
+        const newStart = prompt('Enter a new start date and time (YYYY-MM-DDTHH:MM):', new Date(task.start).toISOString().slice(0, 16));
+        if (newStart) {
+          task.start = new Date(newStart).getTime();
+          const newEnd = prompt('Enter a new end date and time (YYYY-MM-DDTHH:MM):', new Date(task.end).toISOString().slice(0, 16));
+          if (newEnd) {
+            task.end = new Date(newEnd).getTime();
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+            name.textContent = 'Name: ' + task.name;
+            description.textContent = 'Description: ' + task.description;
+            start.textContent = 'Start date and time: ' + new Date(task.start).toLocaleString();
+            end.textContent = 'End date and time: ' + new Date(task.end).toLocaleString();
+          }
+        }
+      }
+    }
+  });
+  
+  taskList.appendChild(li);
+});
+
+// add event listener to clear button
+const clearBtn = document.querySelector('#clear-btn');
+clearBtn.addEventListener('click', function() {
+  localStorage.removeItem('tasks');
+  location.reload();
+});
+
+
+
