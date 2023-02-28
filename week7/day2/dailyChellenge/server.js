3b7b1a303f0650e9476f2845afe75489f4c69e2c
coprum96 <medn@list.ru>
2023-02-28 08:56:54 +0300

    new

--- week7/day2/dailyChellenge/server.js
+++ week7/day2/dailyChellenge/server.js
@@ -1,36 +1,34 @@
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
-app.use('/', express.static(__dirname + '/public'));
-app.use(cors());
-app.use(express.json());
+const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/aboutMe/:hobby', (req, res) => {
   const hobby = req.params.hobby;
-  if (typeof hobby === 'string') {
-    res.send(`My hobby is ${hobby}`);
+  if (typeof hobby !== 'string') {
+    res.status(404).send('Invalid hobby');
   } else {
-    return res.status(404).json({ msg: 'not found' });
+    res.send(`One of my hobbies is ${hobby}`);
   }
 });
 
 app.get('/pic', (req, res) => {
-  res.sendFile(__dirname + '/public/pic.html');
+  res.sendFile(path.join(__dirname, 'public', 'picture.html'));
 });
 
 app.get('/form', (req, res) => {
-  res.sendFile(__dirname + '/public/form.html');
+  res.sendFile(path.join(__dirname, 'public', 'form.html'));
 });
 
 app.post('/formData', (req, res) => {
-  const { email, message } = req.body;
-  if (email || message) {
-    return res.status(400).json({ msg: 'Please include email and message' });
-  }
-  res.send(`${email} sent you a message: "${message}"`);
+  const email = req.body.email;
+  const message = req.body.message;
+  res.send(`${email} sent you a message "${message}"`);
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
-});
+app.listen(3000, () => console.log('Server started on port 3000'));
+
