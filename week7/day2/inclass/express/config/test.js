14efb7ee8b5b7fee6571de93c683a24c434756c4
coprum96 <medn@list.ru>
2023-03-01 09:14:42 +0300

    new

--- week7/day2/inclass/express/config/test.js
+++ week7/day2/inclass/express/config/test.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
 
+
+// try {
+//     const data = fs.readFileSync('.env', 'utf-8');
+//     console.log(data);
+//   } catch (err) {
+//     console.error(err);
+//   }
+
 fs.readFile('.env', 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
