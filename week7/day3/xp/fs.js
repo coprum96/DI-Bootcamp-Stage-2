c6cc64ee1fb1561079bb8ad8d455076b645c631b
coprum96 <medn@list.ru>
2023-03-01 09:20:45 +0300

    new

--- week7/day3/xp/fs.js
+++ week7/day3/xp/fs.js
@@ -1,33 +1,28 @@
-// Exercise 1 : Reading From A File In Node.JS
-// Instructions
-// Create a text file and write something inside (example: ‘Some Text To See’)
-// Create an fs.js file, and inside use the Node JS File System to read the text file and console.log the output in the terminal
 const fs = require('fs');
 
-fs.readFile('myTextFile.txt', 'utf8', (err, data) => {
+// Create a new text file
+fs.writeFile('myFile.txt', 'Hello World!', (err) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(data);
-});
-// Exercise 2 : Writing Files With Node JS
-// Instructions
-// Create an fs.js file, and use the Node js File System to create a new text file and write to it.
-// Use the Node js File System to append some other text to the text file. (Example:”Buy orange juice”)
-// Use the Node js File System to delete the file.
-
-const fs = require('fs');
-
-fs.writeFile('myFile.txt', 'Hello World!', function (err) {
-  if (err) throw err;
   console.log('File created!');
 });
-fs.appendFile('myFile.txt', '\nBuy orange juice', function (err) {
-    if (err) throw err;
-    console.log('Text appended!');
-  });
-  fs.unlink('myFile.txt', function (err) {
-    if (err) throw err;
-    console.log('File deleted!');
-  });
+
+// Append some text to the text file
+fs.appendFile('myFile.txt', '\nBuy orange juice', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Text appended!');
+});
+
+// Delete the file
+fs.unlink('myFile.txt', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('File deleted!');
+});
