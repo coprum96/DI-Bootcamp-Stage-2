ba08c7d139375b7b2acb4e99bfe0c5e6e65f2484
coprum96 <medn@list.ru>
2023-03-20 15:42:29 +0300

    new

---
+++ week10/day2/memeGenerator/meme-/src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  render() {
+    return (
+      <header>
+        <img
+        src='http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png'
+        alt='Problem?'
+      />
+      <p>MemeGenerator</p>
+      </header>
+    )
+  }
+}
+
+export default Header
